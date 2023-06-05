1e62da7f6de82e49dc220f42c692fed090b256ab
DluccaSR <102255318+DluccaSR@users.noreply.github.com>
2023-06-05 17:16:27 -0300

    Update banco-01-06-2023 Cadastro de Usuario.sql
    adicionei produtos

--- BANCO DE DADOS/banco-01-06-2023 Cadastro de Usuario.sql
+++ BANCO DE DADOS/banco-01-06-2023 Cadastro de Usuario.sql
@@ -8,5 +8,20 @@ CREATE TABLE usuarios (
     email VARCHAR(255) NOT NULL UNIQUE,
     senha_hash VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE produtos (
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
+    nome_produto VARCHAR(255),
+    descricao VARCHAR(255),
+    marca VARCHAR(200),
+    categoria VARCHAR(255),
+    preco DECIMAL(9, 2),
+    quantidade_produto INT,
+    imagem VARCHAR(255),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
 select * from usuarios;
 drop table usuarios;
