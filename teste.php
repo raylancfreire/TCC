ab58fd4dc1f2d15a8e647af449ef3bd2faa18fd4
DluccaSR <102255318+DluccaSR@users.noreply.github.com>
2023-06-10 14:53:36 -0300

    Update teste.php
     Index com catalogo de colunas e linhas

--- teste.php
+++ teste.php
@@ -1,107 +1,106 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 
 <head>
-
-    <title>Tela Inicial</title>
-    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+  <script>
+    function alterarEndereco() {
+      var novoEndereco = prompt("Digite o novo endereço:");
+      if (novoEndereco) {
+        document.getElementById("endereco").textContent = novoEndereco;
+      }
+    }
+  </script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Página Inicial</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="CSS/style.css">
 </head>
 
 <body>
-    <header class="navbar">
-        <div class="logo">
-            <img src="logo.png" alt="Logo">
-        </div>
-        <nav>
-            <ul class="nav-links">
-                <li><a href="#">Explorar</a></li>
-                <li><a href="#">Meus Pedidos</a></li>
-                <li><a href="#">Restaurantes</a></li>
-                <li><a href="#">Ofertas</a></li>
-                <li><a href="#">Perfil do Usuário</a></li>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">ExpCars</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="?page=catalogo">Catálogo</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=ofertas">Ofertas</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Menu
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="?page=perfil">Perfil</a></li>
+              <li><a class="dropdown-item" href="?page=pedidos">Meus Pedidos</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="logout.php">Sair</a></li>
             </ul>
-        </nav>
-        <div class="search-box">
-            <input type="text" placeholder="Pesquisar">
-            <button type="submit">Buscar</button>
+          </li>
+        </ul>
+        <div class="container">
+          <div class="d-flex justify-content-end ms-auto">
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <p id="endereco">Rua Exemplo, 123 - Cidade</p>
+            <button class="btn btn-outline-primary" onclick="alterarEndereco()">Alterar Endereço</button>
+          </div>
         </div>
-    </header>
-
-    <div class="slideshow-container">
-        <div class="slides">
-            <div class="color-square red-square"></div>
-            <div class="color-square green-square"></div>
-            <div class="color-square blue-square"></div>
-        </div>
+      </div>
     </div>
-    <?php
-    require("conn.php");
-    // Consultar os produtos no banco de dados
-    $sql = "SELECT * FROM produtos";
-    $result = $conn->query($sql);
 
-    // Exibir os produtos na <div>
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $produtoHTML = '
-            <div class="produto">
-                <img src="' . $row["imagem"] . '" alt="Imagem do produto">
-                <div class="nome">' . $row["nome_produto"] . '</div>
-                <div class="preco">R$ ' . $row["preco"] . '</div>
-                <button class="comprar">Comprar</button>
-            </div>
-        ';
-            echo $produtoHTML;
-        }
-    } else {
-        echo "Nenhum produto encontrado.";
-    }
-    ?>
+  </nav>
 
+  <div class="container">
+    <div class="row">
+      <div class="col mt-5">
+        <!-- Conteúdo do catálogo -->
+        <div class="catalog">
+          <h1>Catálogo</h1>
+              <?php
+              require("conn.php");
 
-    <!-- Restante do código HTML -->
+              // Consulta os dados da tabela "produtos"
+              $sql = "SELECT path, nome_produto, descricao, preco FROM produtos";
+              $result = $pdo->query($sql);
 
-    <section class="highlight">
-        <!-- Banners promocionais aqui -->
-    </section>
+              // Exibe os produtos em forma de catálogo
+              if ($result->rowCount() > 0) {
+                echo "<div class='catalog'>"; // Início do catálogo
 
-    <section class="food-categories">
-        <!-- Categorias de alimentos aqui -->
-    </section>
+                foreach ($result as $row) {
+                  // Exibe os detalhes do produto
+                  echo "<div class='product'>";
+                  echo "<img class='product-image' src='upload/{$row['path']}' alt='Imagem do produto'>";
+                  echo "<h3 class='product-name'>{$row['nome_produto']}</h3>";
+                  echo "<p class='product-description'>{$row['descricao']}</p>";
+                  echo "<p class='product-price'>Preço: R$ {$row['preco']}</p>";
+                  echo "<button class='botao-comprar'>Comprar</button>";
+                  echo "</div>";
+                }
 
-    <section class="personalized-recommendations">
-        <!-- Recomendações personalizadas aqui -->
-    </section>
+                echo "</div>"; // Fim do catálogo
+              } else {
+                echo "<p>Nenhum produto encontrado.</p>";
+              }
+              ?>
 
-    <section class="restaurant-list">
-        <!-- Lista de restaurantes aqui -->
-    </section>
+        </div>
+      </div>
+    </div>
+  </div>
 
-    <footer class="footer">
-        <!-- Rodapé aqui -->
-    </footer>
-
-    <script>
-        var slideIndex = 0;
-        showSlide();
-
-        function showSlide() {
-            var i;
-            var slides = document.getElementsByClassName("color-square");
-
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-
-            slideIndex++;
-            if (slideIndex > slides.length) {
-                slideIndex = 1;
-            }
-
-            slides[slideIndex - 1].style.display = "block";
-            setTimeout(showSlide, 2000); // Altera o slide a cada 2 segundos (2000 milissegundos)
-        }
-    </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
