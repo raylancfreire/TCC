e22ca71f501c089ab3161bd5eca6328e1966f487
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-13 20:54:32 -0300

    Update cadastrar_produto.php

--- cadastrar_produto.php
+++ cadastrar_produto.php
@@ -5,26 +5,75 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        .center {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
+    <style>   
+        .form-group {
+            margin-bottom: 15px;
+        }
+        
+        .form-group label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        
+        .form-group input[type="text"],
+        .form-group input[type="number"],
+        .form-group input[type="file"],
+        .form-group textarea {
+            width: 100%;
+            padding:5px;
+            border: 2px solid #ccc;
+            border-radius: 8px;
+            box-sizing: border-box;
+        }
+        
+        .form-group textarea {
+            resize: vertical;
+        }
+        
+        .form-group button {
+            background-color: #4CAF50;
+            border: none;
+            color: #fff;
+            padding: 10px 20px;
+            border-radius: 3px;
+            cursor: pointer;
         }
     </style>
 </head>
-<body>
-    <div class="center">
+<body><br><br>
+    <div class="container">
         <form action="CRUD/cad_produto.php" method="post" enctype="multipart/form-data">
-            <input type="text" name="nome_produto" placeholder="Nome do Produto" required><br>
-            <input type="text" name="descricao" placeholder="Descrição" required><br>
-            <input type="text" name="marca" placeholder="Marca" required><br>
-            <input type="text" name="categoria" placeholder="Categoria" required><br>
-            <input type="number" name="preco" placeholder="Preço" step="0.01" required><br>
-            <input type="number" name="quantidade_produto" placeholder="Quantidade do Produto" required><br>
-            <input type="file" name="imagem" required><br>
-            <button type="submit">Enviar</button>
+            <div class="form-group">
+                <label for="nome_produto">Nome do Produto</label>
+                <input type="text" id="nome_produto" name="nome_produto" required>
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição</label>
+                <input type="text" id="descricao" name="descricao" required>
+            </div>
+            <div class="form-group">
+                <label for="marca">Marca</label>
+                <input type="text" id="marca" name="marca" required>
+            </div>
+            <div class="form-group">
+                <label for="categoria">Categoria</label>
+                <input type="text" id="categoria" name="categoria" required>
+            </div>
+            <div class="form-group">
+                <label for="preco">Preço</label>
+                <input type="number" id="preco" name="preco" step="0.01" required>
+            </div>
+            <div class="form-group">
+                <label for="quantidade_produto">Quantidade do Produto</label>
+                <input type="number" id="quantidade_produto" name="quantidade_produto" required>
+            </div>
+            <div class="form-group">
+                <label for="imagem">Imagem</label>
+                <input type="file" id="imagem" name="imagem" required>
+            </div>
+            <div class="form-group">
+                <button type="submit">Enviar</button>
+            </div>
         </form>
     </div>
 </body>
