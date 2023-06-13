f5e16a1db13dc08055011038eb24ea48d82c5f9e
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-13 20:32:38 -0300

    Update cadastrar_produto.php

--- cadastrar_produto.php
+++ cadastrar_produto.php
@@ -5,17 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
 </head>
 <body>
-    <form action="CRUD/cad_produto.php" method="post" enctype="multipart/form-data">
-        <input type="text" name="nome_produto" placeholder="Nome do Produto" required><br>
-        <input type="text" name="descricao" placeholder="Descrição" required><br>
-        <input type="text" name="marca" placeholder="Marca" required><br>
-        <input type="text" name="categoria" placeholder="Categoria" required><br>
-        <input type="number" name="preco" placeholder="Preço" step="0.01" required><br>
-        <input type="number" name="quantidade_produto" placeholder="Quantidade do Produto" required><br>
-        <input type="file" name="imagem" required><br>
-        <button type="submit">Enviar</button>
-    </form>
+    <div class="center">
+        <form action="CRUD/cad_produto.php" method="post" enctype="multipart/form-data">
+            <input type="text" name="nome_produto" placeholder="Nome do Produto" required><br>
+            <input type="text" name="descricao" placeholder="Descrição" required><br>
+            <input type="text" name="marca" placeholder="Marca" required><br>
+            <input type="text" name="categoria" placeholder="Categoria" required><br>
+            <input type="number" name="preco" placeholder="Preço" step="0.01" required><br>
+            <input type="number" name="quantidade_produto" placeholder="Quantidade do Produto" required><br>
+            <input type="file" name="imagem" required><br>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
 </body>
 </html>
