e6ee057794c7f222432418fff8f26ab31c0dd054
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-13 23:35:25 -0300

    Update catalogo.php

--- catalogo.php
+++ catalogo.php
@@ -1,6 +1,10 @@
-</html>
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    $includeNavbar = true;
+    if ($includeNavbar) {
+        include("navbar.php"); // Inclui a navbar
+    }
+    ?>
+<html lang="pt-br">
 <head>
   <link rel="stylesheet" href="catalogo.css">
     <meta charset="UTF-8">
@@ -25,30 +29,30 @@
     </div>
 </body>
 </html>
+
 <?php
+    include("conn.php");
     // Verifica se o formulário de busca foi enviado
     if (isset($_GET['nome_produto'])) {
-        $nomeProduto = $_GET['nome_produto'];
-
-        // Consulta os dados da tabela "produtos" com base no nome do produto
-        $sql = "SELECT path, nome_produto, descricao, preco FROM produtos WHERE nome_produto LIKE '%$nomeProduto%'";
-        $result = $pdo->query($sql);
-
-        // Exibe os produtos em forma de catálogo
-        if ($result->rowCount() > 0) {
-            foreach ($result as $row) {
-                // Exibe os detalhes do produto
-                echo "<div class='product'>";
-                echo "<img class='product-image' src='upload/{$row['path']}' alt='Imagem do produto'>";
-                echo "<h3 class='product-name'>{$row['nome_produto']}</h3>";
-                echo "<p class='product-description'>{$row['descricao']}</p>";
-                echo "<p class='product-price'>Preço: R$ {$row['preco']}</p>";
-                echo "<button class='botao-comprar'>Comprar</button>";
-                echo "</div>";
-            }
-        } else {
-            echo "<p>Nenhum produto encontrado.</p>";
-        }
-    }
-?>
-
+      $nomeProduto = $_GET['nome_produto'];
+      
+      // Consulta os dados da tabela "produtos" com base no nome do produto
+      $sql = "SELECT path, nome_produto, descricao, preco FROM produtos WHERE nome_produto LIKE '%$nomeProduto%'";
+      $result = $pdo->query($sql);
+      
+      // Exibe os produtos em forma de catálogo
+      if ($result->rowCount() > 0) {
+          foreach ($result as $row) {
+              echo "<div class='product'>";
+              echo "<img class='product-image' src='upload/{$row['path']}' alt='Imagem'>";
+              echo "<h3 class='product-name'>{$row['nome_produto']}</h3>";
+              echo "<p class='product-description'>{$row['descricao']}</p>";
+              echo "<p class='product-price'>Preço: R$ {$row['preco']}</p>";
+              echo "<button class='botao-comprar'>Comprar</button>";
+              echo "</div>";
+          }
+      } else {
+          echo "<p>Nenhum produto encontrado.</p>";
+      }
+  }  
+  
